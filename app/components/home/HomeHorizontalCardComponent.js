057e11738564b3cc77b6b6ffcc519ab1235a9aa0
Kimsanlim <kimsanlim27@gmail.com>
2022-08-22 10:04:08 +0700

    Add image to Home horizontal card component

--- app/components/home/HomeHorizontalCardComponent.js
+++ app/components/home/HomeHorizontalCardComponent.js
@@ -1,28 +1,31 @@
 import React from 'react';
-import { TouchableOpacity, StyleSheet } from 'react-native';
+import { TouchableOpacity, View, StyleSheet } from 'react-native';
+import { Card } from 'react-native-paper';
 
 import HomeHorizontalCardImageComponent from './HomeHorizontalCardImageComponent';
 import HomeHorizontalCardInfoComponent from './HomeHorizontalCardInfoComponent';
-import { cardElevation } from '../../constants/component_constant';
+import { cardElevation, cardBorderRadius } from '../../constants/component_constant';
 
 const HomeHorizontalCardComponent = (props) => {
   return (
-    <TouchableOpacity style={[styles.container, props.containerStyle]}>
-      <HomeHorizontalCardImageComponent />
-
-      <HomeHorizontalCardInfoComponent />
-    </TouchableOpacity>
+    <Card mode="elevated" elevation={cardElevation} style={[styles.container, props.containerStyle]}
+      onPress={() => console.log('on press card ===')}
+    >
+      <View style={{flexDirection: 'row'}}>
+        <HomeHorizontalCardImageComponent />
+        <HomeHorizontalCardInfoComponent />
+      </View>
+    </Card>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    borderRadius: 10,
-    elevation: cardElevation,
+    borderRadius: cardBorderRadius,
     flexDirection: 'row',
     height: 120,
-    // paddingHorizontal: 12,
-    paddingLeft: 12
+    paddingLeft: 12,
+    paddingRight: 4,
   }
 });
 
--- app/components/home/HomeHorizontalCardImageComponent.js
+++ app/components/home/HomeHorizontalCardImageComponent.js
@@ -1,12 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { ImageBackground, Image, View, Text, StyleSheet } from 'react-native';
+
+import { cardBorderRadius } from '../../constants/component_constant';
 
 const HomeHorizontalCardImageComponent = () => {
   return (
-    <View style={{flex: 1}}>
-      <Text>Image</Text>
+    <View style={styles.container}>
+      <ImageBackground source={require('../../assets/images/intro_1.jpg')} resizeMode='cover'
+        style={styles.image} imageStyle={{borderRadius: cardBorderRadius}}
+      />
+      
+      {/* <View style={styles.imageContainer}>
+        <Image source={require('../../assets/images/intro_1.jpg')} style={styles.image} resizeMode='cover' />
+      </View> */}
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    height: 120,
+    position: 'relative',
+  },
+  image: {
+    borderRadius: cardBorderRadius,
+    elevation: 4,
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: -16,
+  }
+});
 
 export default HomeHorizontalCardImageComponent;
