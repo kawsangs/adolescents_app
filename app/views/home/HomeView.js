62111497aa81c80303674cc52e801343ea980dc2
Kimsanlim <kimsanlim27@gmail.com>
2022-08-30 17:28:25 +0700

    Apply gradient color to audio player header

--- app/views/home/HomeView.js
+++ app/views/home/HomeView.js
@@ -1,12 +1,29 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
 const HomeView = () => {
   return (
     <View style={{flex: 1}}>
       <Text>Home screen</Text>
+
+      <LinearGradient
+        colors={['#347cb6', 'rgba(170, 73, 133, 0.88)']}
+        start={{x: 0, y: 0}} end={{x: 1, y: 0}}
+        style={styles.linearGradient}
+      >
+      </LinearGradient>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  linearGradient: {
+    height: 100,
+    paddingLeft: 15,
+    paddingRight: 15,
+    borderRadius: 5
+  },
+});
 
 export default HomeView
