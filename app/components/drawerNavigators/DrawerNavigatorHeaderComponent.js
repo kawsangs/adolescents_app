3bac61aa2f7e6ef72e3b6d94da4aad3412c4c852
Kimsanlim <kimsanlim27@gmail.com>
2022-09-12 17:02:57 +0700

    Create anonymous icon and translate number

--- app/components/drawerNavigators/DrawerNavigatorHeaderComponent.js
+++ app/components/drawerNavigators/DrawerNavigatorHeaderComponent.js
@@ -4,17 +4,25 @@ import {Text} from 'react-native-paper';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
 import GradientViewComponent from '../shared/GradientViewComponent';
+import AnonymousIconComponent from '../shared/AnonymousIconComponent';
 import color from '../../themes/color';
 import {largeFontSize} from '../../utils/font_size_util';
 
 const DrawerNavigatorHeaderComponent = (props) => {
+  const renderIcon = () => {
+    return props.isAnonymous ? <AnonymousIconComponent size={29} color={color.whiteColor}/>
+                             : <FeatherIcon name='user' color={color.whiteColor} size={29} />
+  }
+
   return (
     <View style={{flexDirection: 'row', borderWidth: 0, marginTop: 40, alignItems: 'center'}}>
       <GradientViewComponent style={{ width: 64, height: 64, borderRadius: 30, justifyContent: 'center', alignItems: 'center' }}>
-        <FeatherIcon name='user' color={color.whiteColor} size={29} />
+        {renderIcon()}
       </GradientViewComponent>
 
-      <Text style={{color: color.whiteColor, marginLeft: 16, fontSize: largeFontSize()}}>ប្រុស | ២០ឆ្នាំ</Text>
+      { !props.isAnonymous &&
+        <Text style={{color: color.whiteColor, marginLeft: 16, fontSize: largeFontSize()}}>ប្រុស | ២០ឆ្នាំ</Text>
+      }
     </View>
   )
 }
