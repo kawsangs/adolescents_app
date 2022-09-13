feb7301de319c892ee647c3a4c5b05d20a8fa318
Kimsanlim <kimsanlim27@gmail.com>
2022-09-13 11:28:45 +0700

    Render the titled card dynamically from local cateogries data

--- app/components/shared/HorizontalCardComponent.js
+++ app/components/shared/HorizontalCardComponent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import {Card} from 'react-native-paper';
 
 import HorizontalCardImageComponent from './horizontalCard/HorizontalCardImageComponent';
