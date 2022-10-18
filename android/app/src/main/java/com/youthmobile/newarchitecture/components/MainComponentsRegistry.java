2bc895e75c334fceb73218856b6fbb1cc3f75faf
Kimsanlim <kimsanlim27@gmail.com>
2022-10-18 14:13:00 +0700

    Update the app package name

--- android/app/src/main/java/com/youthmobile/newarchitecture/components/MainComponentsRegistry.java
+++ android/app/src/main/java/com/youthmobile/newarchitecture/components/MainComponentsRegistry.java
@@ -1,4 +1,4 @@
-package com.youthmobile.newarchitecture.components;
+package kh.org.childhelpline.youthhealth.newarchitecture.components;
 
 import com.facebook.jni.HybridData;
 import com.facebook.proguard.annotations.DoNotStrip;
