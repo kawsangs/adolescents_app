314e8eabe366240a8b309816a921d70b01587af5
Kimsanlim <kimsanlim27@gmail.com>
2022-10-26 11:43:46 +0700

    Record and send visit API when press on the video, service, and consulting bottom tabs

---
+++ app/constants/bottom_tab_constant.js
@@ -0,0 +1,5 @@
+export const tabVisitParams = {
+  video: { code: "video", name: "វីដេអូ", parent_code: null },
+  service: { code: "service", name: "សេវា", parent_code: null },
+  consulting: { code: "consulting", name: "ប្រឹក្សា", parent_code: null }
+}
