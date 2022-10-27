1b75a2d3bce1435ee9b35162a0facdb279fa3fdf
Kimsanlim <kimsanlim27@gmail.com>
2022-10-27 17:18:23 +0700

    Move the horizontal list of facilities to shared component

--- app/components/consultingDetails/ConsultingDetailMainComponent.android.js
+++ app/components/consultingDetails/ConsultingDetailMainComponent.android.js
@@ -1,40 +1,30 @@
 import React from 'react';
-import {View, Dimensions, ScrollView} from 'react-native';
+import {View} from 'react-native';
 import {Text} from 'react-native-paper';
+import {useTranslation} from 'react-i18next';
 
 import ConsultingDetailDescriptionComponent from './ConsultingDetailDescriptionComponent';
-import FacilityCardItemComponent from '../facilities/FacilityCardItemComponent';
+import FacilityHorizontalListComponent from '../shared/FacilityHorizontalListComponent';
+
 import Facility from '../../models/Facility';
 import {screenHorizontalPadding} from '../../constants/component_constant';
 
 const ConsultingDetailMainComponent = () => {
+  const {t} = useTranslation();
   const [facilities, setFacilities] = React.useState(Facility.getAll());
   const [playingUuid, setPlayingUuid] = React.useState(null);
-
-  const renderFacilities = () => {
-    return facilities.map((facility, index) => {
-      return <FacilityCardItemComponent key={index} facility={facility}
-                playingUuid={playingUuid}
-                updatePlayingUuid={(uuid) => setPlayingUuid(uuid)}
-                containerStyle={{width: Dimensions.get('screen').width - 32, marginTop: 0, marginRight: 8}}
-             />
-    });
-  }
 
   return (
     <React.Fragment>
       <View style={{paddingTop: 16, paddingHorizontal: screenHorizontalPadding}}>
         <ConsultingDetailDescriptionComponent/>
-
-        <Text style={{marginTop: 20, marginBottom: 8, color: 'white', fontSize: 16}}>ណែនាំកន្លែងផ្តល់សេវា</Text>
+        <Text style={{marginTop: 20, marginBottom: 8, color: 'white', fontSize: 16}}>{t('recommendedServiceProvider')}</Text>
       </View>
-      <ScrollView
-        contentContainerStyle={{paddingBottom: 4, paddingLeft: screenHorizontalPadding, paddingRight: 8}}
-        style={{flexGrow: 0, width: '100%'}}
-        horizontal={true}
-      >
-        { renderFacilities() }
-      </ScrollView>
+      <FacilityHorizontalListComponent
+        facilities={facilities}
+        playingUuid={playingUuid}
+        updatePlayingUuid={(uuid) => setPlayingUuid(uuid)}
+      />
     </React.Fragment>
   )
 }
