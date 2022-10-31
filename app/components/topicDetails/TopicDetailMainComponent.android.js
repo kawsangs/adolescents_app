8ef73f6ff6d61ed28e88778ec60ec429321c23fe
Kimsanlim <kimsanlim27@gmail.com>
2022-10-31 16:01:32 +0700

    Update the facilities list to vertical scroll

--- app/components/topicDetails/TopicDetailMainComponent.android.js
+++ app/components/topicDetails/TopicDetailMainComponent.android.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, Dimensions} from 'react-native';
 import {Text} from 'react-native-paper';
 import {useTranslation} from 'react-i18next';
 
 import TopicDetailDescriptionComponent from './TopicDetailDescriptionComponent';
-import FacilityHorizontalListComponent from '../shared/FacilityHorizontalListComponent';
+import FacilityCardItemComponent from '../facilities/FacilityCardItemComponent';
 
 import {screenHorizontalPadding} from '../../constants/component_constant';
 import topicHelper from '../../helpers/topic_helper';
@@ -14,17 +14,23 @@ const TopicDetailMainComponent = (props) => {
   const [facilities] = React.useState(topicHelper.getFacilitiesByTopic(props.topicUuid));
   const [playingUuid, setPlayingUuid] = React.useState(null);
 
+  const renderFacilities = () => {
+    return facilities.map((facility, index) => {
+      return <FacilityCardItemComponent key={index} facility={facility}
+                playingUuid={playingUuid}
+                updatePlayingUuid={(uuid) => setPlayingUuid(uuid)}
+                containerStyle={{width: Dimensions.get('screen').width - 32}}
+             />
+    });
+  }
+
   return (
     <React.Fragment>
       <View style={{paddingTop: 16, paddingHorizontal: screenHorizontalPadding}}>
         <TopicDetailDescriptionComponent uuid={props.uuid} type={props.type} />
-        { facilities.length > 0 && <Text style={{marginTop: 20, marginBottom: 8, color: 'white', fontSize: 16}}>{t('recommendedServiceProvider')}</Text>}
+        { facilities.length > 0 && <Text style={{marginTop: 20, marginBottom: 0, color: 'white', fontSize: 16}}>{t('recommendedServiceProvider')}</Text>}
+        { renderFacilities() }
       </View>
-      <FacilityHorizontalListComponent
-        facilities={facilities}
-        playingUuid={playingUuid}
-        updatePlayingUuid={(uuid) => setPlayingUuid(uuid)}
-      />
     </React.Fragment>
   )
 }
