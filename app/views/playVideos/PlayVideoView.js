cfd617eb9dd5bb5185673aa4a3b1e25443fb8fd3
Kimsanlim <kimsanlim27@gmail.com>
2022-11-17 13:53:05 +0700

    Allow to swipe up or down to exit video screen and show loading sign while the video is loading

--- app/views/playVideos/PlayVideoView.js
+++ app/views/playVideos/PlayVideoView.js
@@ -1,7 +1,8 @@
 import React, {useEffect, useState} from 'react';
-import {View} from 'react-native';
+import {View, ActivityIndicator} from 'react-native';
 import YoutubePlayer from "react-native-youtube-iframe";
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import  {PanGestureHandler} from 'react-native-gesture-handler'
 
 import PlayVideoHeaderComponent from '../../components/playVideos/PlayVideoHeaderComponent';
 import PlayVideoWarningMessageComponent from '../../components/playVideos/PlayVideoWarningMessageComponent';
@@ -9,10 +10,12 @@ import color from '../../themes/color';
 import Video from '../../models/Video';
 import youtubeHelper from '../../helpers/youtube_helper';
 import networkService from '../../services/network_service';
+import {navigationRef} from '../../navigators/app_navigator';
 
 const PlayVideoView = (props) => {
   const video = Video.findByUuid(props.route.params.uuid);
   const [hasInternet, setHasInternet] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     networkService.checkConnection(() => setHasInternet(true), () => setHasInternet(false));
   }, []);
@@ -25,13 +28,17 @@ const PlayVideoView = (props) => {
     if (!video.url || !hasInternet)
       return <PlayVideoWarningMessageComponent hasVideo={!!video.url} />;
 
-    return <View style={{flex: 1, justifyContent: 'center'}}>
+    return <PanGestureHandler onGestureEvent={(evt) => navigationRef.current?.goBack()} activeOffsetY={[-70, 70]}>
+            <View style={{flex: 1, justifyContent: 'center'}}>
+              { (hasInternet && isLoading) && <ActivityIndicator size="large" color={color.whiteColor} style={{position: 'absolute', alignSelf: 'center'}} /> }
               <YoutubePlayer
                 height={hp("37%")}
                 play={true}
                 videoId={youtubeHelper.getVideoId(video.url)}
+                onReady={() => setIsLoading(false)}
               />
-           </View>
+            </View>
+          </PanGestureHandler>
   }
 
   return (
