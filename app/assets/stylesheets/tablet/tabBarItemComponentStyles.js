729b83e7f80bdd4f9ed6cb212b8794baa770a15f
Kimsanlim <kimsanlim27@gmail.com>
2022-11-22 11:57:47 +0700

    Update the style of the bottom tab navigator for iOS

--- app/assets/stylesheets/tablet/tabBarItemComponentStyles.js
+++ app/assets/stylesheets/tablet/tabBarItemComponentStyles.js
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import {StyleSheet, Platform} from 'react-native';
 import {smallFontSize} from '../../../utils/font_size_util';
 
 const TabBarItemComponentStyles = StyleSheet.create({
@@ -20,7 +20,14 @@ const TabBarItemComponentStyles = StyleSheet.create({
     alignSelf: 'stretch',
     borderRadius: 6,
     height: 2.1,
-    marginBottom: 0.6
+    ...Platform.select({
+      ios: {
+        marginBottom: 20,
+      },
+      android: {
+        marginBottom: 0.6,
+      }
+    })
   }
 });
 
