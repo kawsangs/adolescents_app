bf6d010460960db05c19072e2725976321e70738
Kimsanlim <kimsanlim27@gmail.com>
2022-11-25 12:59:08 +0700

    Remove scroll from notification screen

--- app/views/notifications/NotificationView.ios.js
+++ app/views/notifications/NotificationView.ios.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-import GradientScrollViewComponent from '../../components/shared/GradientScrollViewComponent';
 import NavigationHeaderWithBackButtonComponent from '../../components/shared/NavigationHeaderWithBackButtonComponent';
 import ComingSoonMessageComponent from '../../components/shared/ComingSoonMessageComponent';
 
 const NavigationView = () => {
   return (
-    <GradientScrollViewComponent
-      header={<NavigationHeaderWithBackButtonComponent label={"ការជូនដំណឹង"} />}
-      body={<ComingSoonMessageComponent/>}
-      scrollViewStyle={{paddingBottom: 0, paddingHorizontal: 0}}
-    />
+    <React.Fragment>
+      <NavigationHeaderWithBackButtonComponent label={"ការជូនដំណឹង"} />
+      <ComingSoonMessageComponent/>
+    </React.Fragment>
   )
 }
 
