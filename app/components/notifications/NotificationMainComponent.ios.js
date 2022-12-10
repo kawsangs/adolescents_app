f4277a69860ab819743a88aa019458f35045727f
Kimsanlim <kimsanlim27@gmail.com>
2022-12-11 01:49:56 +0700

    Create No notification screen

--- app/components/notifications/NotificationMainComponent.ios.js
+++ app/components/notifications/NotificationMainComponent.ios.js
@@ -2,24 +2,18 @@ import React from 'react';
 import {FlatList} from 'react-native';
 
 import NotificationCardItemComponent from './NotificationCardItemComponent';
-import ComingSoonMessageComponent from '../shared/ComingSoonMessageComponent';
+import NotificationEmptyListComponent from './NotificationEmptyListComponent';
 import {screenHorizontalPadding} from '../../constants/component_constant';
-import {gradientScrollViewPaddingBottom} from '../../constants/ios_component_constant';
+import {gradientScrollViewBigPaddingBottom} from '../../constants/ios_component_constant';
 
 const NotificationMainComponent = (props) => {
-  const renderEmptyContent = () => {
-    return (
-      <ComingSoonMessageComponent/>
-    )
-  }
-
   if (props.notifications.length) {
     return (
       <FlatList
         data={props.notifications}
         renderItem={({item}) => <NotificationCardItemComponent notification={item} openConfirmModal={(notification) => props.openConfirmModal(notification)} />}
         keyExtractor={item => item.uuid}
-        contentContainerStyle={{paddingHorizontal: screenHorizontalPadding, paddingBottom: gradientScrollViewPaddingBottom}}
+        contentContainerStyle={{paddingHorizontal: screenHorizontalPadding, paddingBottom: gradientScrollViewBigPaddingBottom}}
         onEndReached={() => props.onEndReached()}
         onEndReachedThreshold={0.3}
         ListFooterComponentStyle={{paddingBottom: 300}}
@@ -27,7 +21,7 @@ const NotificationMainComponent = (props) => {
     )
   }
 
-  return renderEmptyContent();
+  return <NotificationEmptyListComponent/>
 }
 
 export default NotificationMainComponent;
