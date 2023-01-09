060e213632911e5525b8230afe32cfcc0a97f324
Kimsanlim <kimsanlim27@gmail.com>
2023-01-09 14:11:06 +0700

    Add reset filter button on the filter bottom sheet

--- app/components/facilities/FacilityFilterLocationBottomSheetComponent.android.js
+++ app/components/facilities/FacilityFilterLocationBottomSheetComponent.android.js
@@ -1,16 +1,19 @@
 import React, {useState} from 'react';
 import {View, TouchableOpacity} from 'react-native';
-import {Text} from 'react-native-paper';
+import {Text, Button} from 'react-native-paper';
 import {useTranslation} from 'react-i18next';
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
 import Icon from 'react-native-vector-icons/Feather';
 
 import BottomSheetModalMainComponent from '../shared/BottomSheetModalMainComponent';
+import BoldLabelComponent from '../shared/BoldLabelComponent';
 import provinces from '../../db/json/provinces';
 import color from '../../themes/color';
 import componentUtil from '../../utils/component_util';
-import { largeFontSize } from '../../utils/font_size_util';
+import { largeFontSize, xLargeFontSize } from '../../utils/font_size_util';
+import {isLowPixelDensityDevice} from '../../utils/responsive_util';
 import {defaultPickerContentHeight} from '../../constants/modal_constant';
+import { screenHorizontalPadding } from '../../constants/component_constant';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { resetSelectedLocation, storeSelectedLocation } from '../../features/facilities/filterFacilityLocationSlice';
@@ -21,13 +24,26 @@ const FacilityFilterLocationBottomSheetComponent = (props) => {
   const filteredProvince = useSelector(state => state.filterFacilityLocation.value)
 
   const onSelectProvince = (province) => {
+    if (!!filteredProvince && filteredProvince == province.value) return;
     props.modalRef.current?.dismiss();
-    if (!!filteredProvince && filteredProvince == province.value) {
-      dispatch(resetSelectedLocation());
-      return;
-    }
-
     dispatch(storeSelectedLocation(province.value))
+  }
+
+  const resetFilter = () => {
+    props.modalRef.current?.dismiss();
+    dispatch(resetSelectedLocation()); 
+  }
+
+  const renderResetBtn = () => {
+    return <View style={{flexDirection: 'row', paddingHorizontal: screenHorizontalPadding, height: isLowPixelDensityDevice() ? 48 : 56, paddingTop: 6, marginBottom: 16, alignItems: 'center'}}>
+              <BoldLabelComponent label={t('selectClinicLocation')} style={{fontSize: xLargeFontSize(), flex: 1}} />
+              <Button icon="refresh-cw" mode="outlined" onPress={() => resetFilter()}
+                style={{borderColor: color.primaryColor, borderWidth: 1.5}}
+                labelStyle={{fontSize: 16}}
+              >
+                {t('filterReset')}
+              </Button>
+           </View>
   }
 
   const renderListItem = () => {
@@ -44,7 +60,7 @@ const FacilityFilterLocationBottomSheetComponent = (props) => {
 
   return (
     <BottomSheetModalMainComponent
-      title={t('selectClinicLocation')}
+      customTitle={renderResetBtn()}
       containerStyle={{height: hp(defaultPickerContentHeight)}}
       scrollViewStyle={{paddingHorizontal: 16}}
     >
