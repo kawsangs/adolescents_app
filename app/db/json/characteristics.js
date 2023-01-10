fa7f46eb5032a20dcb9fbb309fd023b77549895b
Kimsanlim <kimsanlim27@gmail.com>
2023-01-10 15:19:59 +0700

    Update wording on the create account screen

--- app/db/json/characteristics.js
+++ app/db/json/characteristics.js
@@ -2,6 +2,6 @@ import audioSources from '../../constants/audio_source_constant';
 
 export default [
   { "name_km": "មានប័ណ្ណសមធម៌(ប័ណ្ណក្រីក្រ)", "name_en": "ID poor", "value": "PO", "audio": audioSources["0.10.mp3"] },
-  { "name_km": "ជនជាតិដើម", "name_en": "Ethnic minority", "value": "MI", "audio": audioSources["0.11.mp3"] },
+  { "name_km": "ជនជាតិដើមភាគតិច", "name_en": "Indigenous people", "value": "MI", "audio": audioSources["0.11.mp3"] },
   { "name_km": "ជនមានពិការភាព", "name_en": "Disability", "value": "DI", "audio": audioSources["0.12.mp3"]}
 ]
