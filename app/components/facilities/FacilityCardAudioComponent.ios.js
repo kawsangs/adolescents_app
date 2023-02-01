3d980829cfddbb89e1813579dff2c9287b79fcfe
Kimsanlim <kimsanlim27@gmail.com>
2023-02-01 15:50:05 +0700

    Apply audio player button lib to facility card item

--- app/components/facilities/FacilityCardAudioComponent.ios.js
+++ app/components/facilities/FacilityCardAudioComponent.ios.js
@@ -1,31 +1,25 @@
 import React from 'react';
 import {View} from 'react-native';
-import IonIcon from 'react-native-vector-icons/Ionicons';
 import FeatherIcon from 'react-native-vector-icons/Feather';
 
-import PlayAudioComponent from '../shared/PlayAudioComponent';
+import CustomAudioPlayerButtonComponent from '../shared/CustomAudioPlayerButtonComponent';
 import color from '../../themes/color';
-import componentUtil from '../../utils/component_util';
-import {isShortScreenDevice, getStyleOfDevice, isLowPixelDensityDevice} from '../../utils/responsive_util';
+import {getStyleOfDevice, isLowPixelDensityDevice} from '../../utils/responsive_util';
 
 const FacilityCardAudioComponent = (props) => {
+  const renderAudioBtn = () => {
+    return <CustomAudioPlayerButtonComponent
+              audio={props.audio}
+              itemUuid={props.uuid}
+              playingUuid={props.playingUuid}
+              updatePlayingUuid={props.updatePlayingUuid}
+              accessibilityLabel={props.accessibilityLabel}
+           />
+  }
+
   return (
     <View>
-      <PlayAudioComponent
-        playIcon='volume-high-outline'
-        pauseIcon='pause'
-        muteIcon='volume-mute-outline'
-        isSpeakerIcon={true}
-        iconSize={24}
-        audio={props.audio}
-        btnStyle={[{ borderWidth: 0 }, isShortScreenDevice() && {width: componentUtil.pressableItemSize(), height: componentUtil.pressableItemSize()}]}
-        itemUuid={props.uuid}
-        playingUuid={props.playingUuid}
-        updatePlayingUuid={props.updatePlayingUuid}
-        accessibilityLabel={props.accessibilityLabel}
-      >
-        <IonIcon/>
-      </PlayAudioComponent>
+      {renderAudioBtn()}
       <FeatherIcon name="chevron-right" color={color.primaryColor} size={getStyleOfDevice(32, 30)} style={{alignSelf: 'center', marginTop: getStyleOfDevice(-6, isLowPixelDensityDevice() ? -6 : -12) }} />
     </View>
   )
