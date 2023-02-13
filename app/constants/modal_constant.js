4b1fc925550cca4cd593694fcd2083d9052d4620
Kimsanlim <kimsanlim27@gmail.com>
2023-02-13 14:43:00 +0700

    Update the height of confirmation bottom sheet for Android medium pixel mobile

--- app/constants/modal_constant.js
+++ app/constants/modal_constant.js
@@ -1,7 +1,7 @@
 import {Platform} from 'react-native';
 import DeviceInfo from 'react-native-device-info';
 import { heightPercentageToDP as hp } from 'react-native-responsive-screen';
-import {getStyleOfDevice} from '../utils/responsive_util';
+import {getStyleOfDevice, isLowPixelDensityDevice} from '../utils/responsive_util';
 
 export const defaultPickerSnapPoints = getStyleOfDevice(['60%'], ['65%']);
 export const defaultPickerContentHeight = hp('55%');
@@ -10,7 +10,8 @@ export const contactContentHeight = getStyleOfDevice('27%', '37.5%');
 export const servicesSnapPoints = ['38%'];
 export const servicesContentHeight = '33%';
 
-const iOSMobileSnappoints = DeviceInfo.hasNotch() ? ['52%'] : ['56%']
-const iOSMobileContentHeight = DeviceInfo.hasNotch() ? ['47%'] : ['51%']
-export const signUpConfirmationSnapPoints = Platform.OS == 'ios' ? getStyleOfDevice(['35%'], iOSMobileSnappoints) : getStyleOfDevice(['42%'], ['59%']);
-export const signUpConfirmationContentHeight = Platform.OS == 'ios' ? getStyleOfDevice('30%', iOSMobileContentHeight) : getStyleOfDevice('37%', '53%');
+
+const confirmationiOSMobie = DeviceInfo.hasNotch() ? {snapPoints: ['52%'], height: '47%'} : {snapPoints: ['56%'], height: '51%'}
+const confirmationAndroidMobile = isLowPixelDensityDevice() ? {snapPoints: ['59%'], height: '53%'} : {snapPoints: ['55%'], height: '49%'}
+export const signUpConfirmationSnapPoints = Platform.OS == 'ios' ? getStyleOfDevice(['35%'], confirmationiOSMobie.snapPoints) : getStyleOfDevice(['42%'], confirmationAndroidMobile.snapPoints);
+export const signUpConfirmationContentHeight = Platform.OS == 'ios' ? getStyleOfDevice('30%', confirmationiOSMobie.height) : getStyleOfDevice('37%', confirmationAndroidMobile.height);
