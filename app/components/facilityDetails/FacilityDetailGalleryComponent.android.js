df6834124548aa96937e2135eed67287a3019ef9
Kimsanlim <kimsanlim27@gmail.com>
2023-02-16 15:35:03 +0700

    Show clinic logo in clinic detail screen and remove react-native-swiper

--- app/components/facilityDetails/FacilityDetailGalleryComponent.android.js
+++ app/components/facilityDetails/FacilityDetailGalleryComponent.android.js
@@ -1,32 +1,17 @@
 import React from 'react';
 import {View, Image, StyleSheet} from 'react-native';
-import Swiper from 'react-native-swiper'
 
 import EmptyImageComponent from '../shared/EmptyImageComponent';
-import color from '../../themes/color';
 import Facility from '../../models/Facility';
 import {getStyleOfDevice} from '../../utils/responsive_util';
+import imageSources from '../../constants/image_source_constant';
 
 const FacilityDetailGalleryComponent = (props) => {
-  const [galleries] = React.useState(Facility.findByUuid(props.uuid).galleries);
-  const renderImages = () => {
-    return galleries.map((gallery, index) => {
-      return <Image key={index} source={gallery} style={{width: '100%', height: '100%'}} />
-    });
-  }
-
-  const renderImageSlider = () => {
-    return <Swiper style={styles.wrapper} showsButtons={false}
-              dotStyle={{bottom: -20, backgroundColor: '#818181'}}
-              activeDotStyle={{bottom: -20, backgroundColor: color.secondaryColor}}
-            >
-              {renderImages()}
-            </Swiper>
-  }
-
+  const logo = Facility.findByUuid(props.uuid).logo;
+  const logoSize = getStyleOfDevice(160, 140)
   return (
-    <View style={{height: galleries.length > 0 ? getStyleOfDevice(260, 216) : getStyleOfDevice(260, 180)}}>
-      { galleries.length > 0 ? renderImageSlider()
+    <View style={{height: getStyleOfDevice(220, 180), justifyContent: 'center', alignItems: 'center'}}>
+      { logo ? <Image source={imageSources[logo]} style={{width: logoSize, height: logoSize}} />
         : <EmptyImageComponent iconSize={getStyleOfDevice(32, 40)} labelStyle={{fontSize: 12}} iconContainerStyle={styles.emptyIconContainer} />
       }
     </View>
@@ -34,29 +19,6 @@ const FacilityDetailGalleryComponent = (props) => {
 }
 
 const styles = StyleSheet.create({
-  slide1: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
-  },
-  slide2: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#97CAE5'
-  },
-  slide3: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#92BBD9'
-  },
-  text: {
-    color: '#fff',
-    fontSize: 30,
-    fontWeight: 'bold'
-  },
   emptyIconContainer: {
     paddingHorizontal: getStyleOfDevice(20, 18),
     paddingVertical: getStyleOfDevice(26, 20)
