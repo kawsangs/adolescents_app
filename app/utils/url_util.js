0c2c1b633b771b39f7be1bfb62cddf406a684b0e
Kimsanlim <kimsanlim27@gmail.com>
2023-03-13 13:26:23 +0700

    Update the URL regular expression

--- app/utils/url_util.js
+++ app/utils/url_util.js
@@ -27,37 +27,24 @@ const urlUtil = (() => {
   }
 
   function isUrlWithHostname(url) {
-    const ipAddressUrl = new RegExp(`^https?://${_ipAddressPattern()}[:]?`).exec(url)
-    const localhostUrl = new RegExp(`^https?://localhost[:]?`).exec(url)
-    const domainUrl = new RegExp(`^https?://${_hostnamePattern()}[:]?`).exec(url)
-    let result = null;
-
-    if (!!ipAddressUrl)
-      result = ipAddressUrl[0];
-    else if (!!localhostUrl)
-      result = localhostUrl[0];
-    else if (!!domainUrl)
-      result = domainUrl[0];
-    else
-      return false
-
-    if (new RegExp(/:$/).test(result))
-      return new RegExp(`^https?://(${_ipAddressPattern()}|${_hostnamePattern()}|localhost):[0-9]{1,4}`).test(url)
-
-    return new RegExp(`^https?://(${_ipAddressPattern()}/|${_hostnamePattern()}|localhost/)`).test(url)
+    return new RegExp(`^https?://(${_hostnamePattern()}|${_ipAddressPattern()}|${_localhostPattern()})`).test(url)
   }
 
   // private method
   function _hostnamePattern() {
     const addressPattern = `[a-z0-9]([-_]{1})?([.]{1})?`;
-    const entityTypePattern = `([.][a-z]{2,})([/])?`;
-    return `(${addressPattern})+${entityTypePattern}`
+    const entityTypePattern = `([.][a-z]{2,})`;
+    return `(${addressPattern})+${entityTypePattern}(:[0-9]{1,4})?([/])?((/([a-z0-9?&=%-_]*))*)?$`
   }
 
   function _ipAddressPattern() {
     const octetsPattern = '[0-9]{1,3}';
-    return `${octetsPattern}[.]${octetsPattern}[.]${octetsPattern}[.]${octetsPattern}`;
+    return `(${octetsPattern}[.]${octetsPattern}[.]${octetsPattern}[.]${octetsPattern})(:[0-9]{1,4})?((/([a-z0-9?&=%-_]*))*)?$`;
   }
-})();
+
+  function _localhostPattern() {
+    return `localhost(:[0-9]{1,4})?((/([a-z0-9?&=%-_]*))*)?$`
+  }
+})()
 
 export default urlUtil;
