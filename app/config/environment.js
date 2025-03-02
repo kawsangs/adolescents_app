f83fe07c0ef735dcd6cb1cfbae49460adbc20d2c
Kimsanlim <kimsanlim27@gmail.com>
2025-03-02 21:36:27 +0700

    Prepare build testing APK

--- app/config/environment.js
+++ app/config/environment.js
@@ -1,13 +1,14 @@
 import { KM } from '../constants/main_constant';
 
 export const environment = {
-  serverUrl: 'http://192.168.1.140:3000',
+  serverUrl: 'https://youthhealth-stg.ilabsea.org',
   type: 'development',
   showIntroSlider: true,
   defaultLanguage: KM,
   sentryDSN: '',
   encryptionKey: '',        // Use 256 bytes key
-  apiKey: '6650308c078ed719a6c4ded9dd092fa0',
+  // apiKey: '6650308c078ed719a6c4ded9dd092fa0',
+  apiKey: 'b33befe315730b4eed94ada294802734',
   isUserBasedAuth: false,
   apiVersion: 'v1',
   isUserBasedApi: false,
